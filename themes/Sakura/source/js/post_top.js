b6b99905757d8e0562fdeee3dbe455122a59bf42
dummerchen <1179502349@qq.com>
2020-12-18 15:17:26 +0800

    时间序列

--- themes/Sakura/source/js/post_top.js
+++ themes/Sakura/source/js/post_top.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
-
-    var pho=bg[Math.floor(Math.random()*bg.length)]
+    rad=Math.floor(Math.random()*bg.length)
+    var pho=bg[rad]
+    console.log(pho,rad)
     t='<div class="pattern-attachment-img lazyload" style="background: url(\''+ pho +'\') center center / cover no-repeat;overflow:hidden;>"></div>'
     $(".pattern-center").append(t)
 })
